003f434158b3fd0d22e8939b16ef34f46bba0a57
John Reinier Bulaong <rein.bulaong@yahoo.com>
2019-05-01 01:41:38 +0800

    Convert main css file to stylus

--- webpack.config.js
+++ webpack.config.js
@@ -4,41 +4,49 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const extractCSS = new ExtractTextPlugin({ filename: "saligtaran.css" });
 
 module.exports = {
-	entry: {
-		index: "./src/index.js",
-		permalink: "./src/permalink.js"
-	},
-	output: {
-		path: path.resolve(__dirname, "./dist"),
-		filename: "[name].js"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: "babel-loader",
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: extractCSS.extract({ // Instance 1
-					fallback: "style-loader",
-					use: [ "css-loader" ]
-				})
-			},
-		]
-	},
-	plugins: [
-		extractCSS,
-		new webpack.optimize.ModuleConcatenationPlugin(),
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true,
-			compress: {
-				warnings: false
-			}
-		}),
-		new webpack.LoaderOptionsPlugin({
-			minimize: true
-		}),
-	]
+  entry: {
+    index: "./src/index.js",
+    permalink: "./src/permalink.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: extractCSS.extract({
+          // Instance 1
+          fallback: "style-loader",
+          use: ["css-loader"]
+        })
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: extractCSS.extract({
+          use: ["css-loader", "stylus-loader"],
+          fallback: "style-loader"
+        })
+      }
+    ]
+  },
+  plugins: [
+    extractCSS,
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]
 };
